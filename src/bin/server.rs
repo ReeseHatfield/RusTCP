0451c52827558cb7426925dada7e15fc6740c7da
ReeseHatfield <hatfield.69@wright.edu>
2025-01-09 13:22:01 -0500

    data modeling

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,6 @@
-use std::{io::{Read, Write}, net::TcpListener};
+use std::{io::{Read, Write}, net::TcpListener, string};
 
-use RusTCP::rustcp;
+use RusTCP::rustcp::{self, buf_to_string, Buffer};
 
 fn main(){
     // a tcp listener is effecitvely just a server
@@ -41,3 +41,45 @@ fn main(){
 
 
 }
+
+
+struct Server{
+    connections: Vec<Connection>,
+    chats: Vec<Chat>
+}
+
+struct Chat(Buffer);
+
+
+impl std::fmt::Display for Chat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+
+        match buf_to_string(&self.0){
+            Ok(s) => write!(f, "{}", s),
+            Err(_) => write!(f, "Error: could not render messsage")
+            
+        }
+        
+    }
+}
+
+
+// a connection TO the TCP server
+struct Connection {
+    client: Socket
+}
+
+
+struct Socket {
+    address: Address,
+    port: Port,
+}
+
+
+// newtype pattern
+struct Address(String);
+// can like impl parse on these and do that cool pattern thingy
+struct Port(u8);
+
+
+
